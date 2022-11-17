d12db07c70b4429b24e690b5a7f238be6ef76598
ew11160 <ea11160@byu.edu>
2022-11-17 10:47:11 -0700

    update

--- trailsapp/admin.py
+++ trailsapp/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from .models import trail_id, trail_name, length_miles, difficulty, completion_time, img_url, description, location
-
+from .models import trails
 # Register your models here.
-admin.site.register(trail_id)
-admin.site.register(trail_name)
-admin.site.register(length_miles)
-admin.site.register(difficulty)
-admin.site.register(completion_time)
-admin.site.register(img_url)
-admin.site.register(description)
-admin.site.register(location)
+
+admin.site.register(trails)
