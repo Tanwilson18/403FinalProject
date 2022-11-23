a0f6451988d0bb7951d814f7ed4ce89cd849882f
Tanwilson18 <totannerwilson@gmail.com>
2022-11-23 12:22:13 -0700

    new upadtes

--- trails/settings.py
+++ trails/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'trails.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'UCTrailsProject',
-        'USER': 'postgres',
-        'PASSWORD': 'eand4851',
-        'HOST': 'localhost',
-        # 'PORT': 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'UCTrailsProject',
+#         'USER': 'postgres',
+#         'PASSWORD': 'eand4851',
+#         'HOST': 'localhost',
+#         # 'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
--- trailsapp/views.py
+++ trailsapp/views.py
@@ -3,16 +3,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-
 # Create your views here.
 def indexPageView(request):
     return render(request, 'trailsapp/index.html')
 
+
 def crudPageView(request):
-    return HttpResponse('This is where our CRUD Page will go')
+    return HttpResponse('trailsapp/crud.html')
+
 
 def featuredPageView(request):
     return HttpResponse('This is where our featured trails will go')
+
 
 def mapPageView(request):
     return HttpResponse('This is where our map Page will go!! YAY FOR MAPS')
