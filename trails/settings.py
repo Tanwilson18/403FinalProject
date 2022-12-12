5aae211f3dab424c879e486b2fda3edae65d0e47
Tanwilson18 <totannerwilson@gmail.com>
2022-12-11 22:27:55 -0700

    stuff

--- trails/settings.py
+++ trails/settings.py
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'trails.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'UCTrailsProject',
-#         'USER': 'postgres',
-#         'PASSWORD': 'eand4851',
-#         'HOST': 'localhost',
-#         # 'PORT': 5432,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trails',
+        'USER': 'postgres',
+        'PASSWORD': 'Medellin',
+        'HOST': 'localhost',
+        # 'PORT': 5432,
+    }
+}
 
 
 # Password validation
--- trailsapp/models.py
+++ trailsapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class trails(models.Model):
-    trail_id = models.IntegerField()
+    trail_id = models.AutoField(primary_key=True)
     trail_name = models.CharField(max_length=50)
     length_miles = models.FloatField()
     difficulty = models.CharField(max_length=15)
@@ -12,3 +12,9 @@ class trails(models.Model):
     img_url = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     location = models.CharField(max_length=75)
+
+    class Meta:
+        db_table = 'trails'
+
+    def __str__(self):
+        return (f'{self.trail_id}')
